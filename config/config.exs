58966bd534e50a2e7b1f59e44aa2bc11e06ac1a7
ante <twogamers1950@gmail.com>
2023-06-15 10:42:37 +0200

    cors

--- config/config.exs
+++ config/config.exs
@@ -31,6 +31,9 @@ config :cors_plug,
     "http://localhost:3002/",
     "http://localhost:3003/",
     "http://localhost:3003",
+    "http://localhost:3003",
+    "http://116.203.201.51:3000"
+    "http://116.203.201.51:3000/"
   ],
   methods: ["GET", "POST", "FETCH", "OPTIONS"]
 
--- config/prod.exs
+++ config/prod.exs
@@ -14,6 +14,7 @@ config :swoosh, local: true
 config :cors_plug,
   origin: [
     "http://116.203.201.51:4000",
+    "http://116.203.201.51:4000/",
     "http://localhost:3002",
     "http://localhost:3003",
     "http://localhost:3003/",
